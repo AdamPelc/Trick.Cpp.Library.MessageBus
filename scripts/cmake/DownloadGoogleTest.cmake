ba9561d07d973e80b03654b422e8e2ba3383afd5
Adam Pelc <adam.pelc.su@gmail.com>
2022-01-06 15:51:51 +0100

    test: Add simple GoogleTest to the project.

---
+++ scripts/cmake/DownloadGoogleTest.cmake
@@ -0,0 +1,9 @@
+message(STATUS "DownloadGoogleTest")
+include(FetchContent)
+FetchContent_Declare(
+        googletest
+        URL https://github.com/google/googletest/archive/release-1.11.0.tar.gz
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
